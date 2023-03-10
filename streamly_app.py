67295dfe24cea673a9abd8c2a4494ba0beaf7c9a
rzafrai <rzafrai@gmail.com>
2023-03-10 18:47:30 +0100

    Update streamly_app.py
    versión definitiva según curso lesson 3

--- streamly_app.py
+++ streamly_app.py
@@ -1,7 +1,5 @@
 import streamlit
-
-streamlit.title('My parents New Healthy Dinner')
-
-streamlit.header('No sé qué estoy haciendo')
-
-streamlit.text('Pues parece que esto está molón, no?')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
