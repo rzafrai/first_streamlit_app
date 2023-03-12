e74f7f5c92bc0d6f309ee54eac019745ba92a619
rzafrai <rzafrai@gmail.com>
2023-03-12 16:48:06 +0100

    Update streamly_app.py

--- streamly_app.py
+++ streamly_app.py
@@ -38,8 +38,8 @@ streamlit.dataframe(fruits_to_show)
 #create the repeatable code blocj /called a function)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    retorn fruityvice_normalized 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized 
   
 #New Section to displau fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
