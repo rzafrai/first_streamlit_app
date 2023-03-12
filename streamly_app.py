051caf0a23096a234eaa15415188e2dd6ec1521b
rzafrai <rzafrai@gmail.com>
2023-03-12 17:59:40 +0100

    Update streamly_app.py

--- streamly_app.py
+++ streamly_app.py
@@ -53,12 +53,12 @@ try:
 
    
 #import snowflake.connector
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx_cursor()
-#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load list contains:")
-#streamlit.dataframe(my_data_rows)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx_cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
