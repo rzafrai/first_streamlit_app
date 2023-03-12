c52312cca82416a9ff09e7908d40546a0b448dcd
rzafrai <rzafrai@gmail.com>
2023-03-12 17:21:23 +0100

    Update streamly_app.py

--- streamly_app.py
+++ streamly_app.py
@@ -61,7 +61,7 @@ try:
 #streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:|")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx_cursor() as my_cur:
