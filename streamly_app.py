0a6bc2cb9a6552f0ae185a51a5f9f6eb2a641fbc
rzafrai <rzafrai@gmail.com>
2023-03-12 17:24:16 +0100

    Update streamly_app.py

--- streamly_app.py
+++ streamly_app.py
@@ -51,7 +51,8 @@ try:
    back_from_function = get_fruityvice_data(fruit_choice)
    streamlit.dataframe(back_from_function)
  
-
+except URLError as e:
+    streamlit.error()
 
 #import snowflake.connector
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
