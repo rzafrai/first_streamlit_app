89bc2ffa36347cce3d2765e7e6d22f4157e34111
rzafrai <rzafrai@gmail.com>
2023-03-12 18:04:42 +0100

    Update streamly_app.py

--- streamly_app.py
+++ streamly_app.py
@@ -60,7 +60,7 @@ try:
 #streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 
-streamlit.header('The fruit load list contains:')
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx_cursor() as my_cur:
@@ -73,8 +73,8 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
